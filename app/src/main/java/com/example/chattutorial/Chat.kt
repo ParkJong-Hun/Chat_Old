69ada7669301e97e5500e6130ef511107650d8f5
ParkJong-Hun <thesn515@gmail.com>
2021-06-16 12:57:49 +0900

    2021/06/16 12:57 First Commit: initialized app

---
+++ app/src/main/java/com/example/chattutorial/Chat.kt
@@ -0,0 +1,11 @@
+package com.example.chattutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//This is Chat Activity
+class Chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chat)
+    }
+}
